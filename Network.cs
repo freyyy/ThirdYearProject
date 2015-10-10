e3496969480b6b053b53d6631a644640e6512e3a
Stefan Andrei Balas <andreibalas93@gmail.com>
2015-10-10 14:16:01 +0100

    Added Network class to group several Layers into a single network.

---
+++ Network.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdYearProject
+{
+    class Network
+    {
+        public Layer[] Layers { set; get; }
+
+        public double[] Output;
+
+        public Network(Layer[] layers)
+        {
+            Layers = layers;
+        }
+    }
+}
