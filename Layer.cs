a8a9c65aea10013f0c5b2baf043476ec92a16a1b
Stefan Andrei Balas <andreibalas93@gmail.com>
2015-10-10 13:37:40 +0100

    Added Layer class to group Neurons into layers.

---
+++ Layer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdYearProject
+{
+    class Layer
+    {
+        public Neuron[] Neurons { set; get; }
+
+        public int InputCount;
+
+        public int NeuronCount;
+
+        public Layer(int neuronCount, int inputCount, ActivationFunction function)
+        {
+            InputCount = inputCount;
+            NeuronCount = neuronCount;
+            Neurons = new Neuron[neuronCount];
+            for(int i = 0; i < neuronCount; i++)
+            {
+                Neurons[i] = new Neuron(inputCount, function);
+            }
+        }
+
+        public double[] Output(double[] inputs)
+        {
+            double[] output = new double[NeuronCount];
+
+            for(int i = 0; i < NeuronCount; i++)
+            {
+                output[i] = Neurons[i].Output(inputs);
+            }
+
+            return output;
+        }
+    }
+}
