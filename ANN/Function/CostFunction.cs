ec1cb7861a28aee13a1fd9fbc7744585f64e2df6
Stefan Andrei Balas <andreibalas93@gmail.com>
2015-12-08 18:29:22 +0000

    Added definition for a cost function

---
+++ ANN/Function/CostFunction.cs
@@ -0,0 +1,8 @@
+﻿namespace ANN.Function
+{
+    public interface CostFunction
+    {
+        double ComputeCost(double actual, double target);
+        double ComputeAverageCost(double[] actual, double[] target);
+    }
+}
---
+++ ANN/Function/SquaredErrorCostFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANN.Function
+{
+    public class SquaredErrorCostFunction : CostFunction
+    {
+        public SquaredErrorCostFunction()
+        {
+        }
+
+        public double ComputeAverageCost(double[] actual, double[] target)
+        {
+            if (actual.Length != target.Length)
+            {
+                throw new ArgumentException("Invalid vectors for squared error cost. Make sure they are of the same length.");
+            }
+            return 0.5 * actual.Zip(target, (a, t) => Math.Pow((a - t), 2)).Average();
+        }
+
+        public double ComputeCost(double actual, double target)
+        {
+            return 0.5 * Math.Pow((actual - target), 2);
+        }
+    }
+}
