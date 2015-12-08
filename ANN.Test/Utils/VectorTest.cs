847b0b192e67365a28d2867f42b9cb3434d04f52
Stefan Andrei Balas <andreibalas93@gmail.com>
2015-12-08 18:34:32 +0000

    Unit tests for vector operations

---
+++ ANN.Test/Utils/VectorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ANN.Utils;
+
+namespace ANN.Test.Utils
+{
+    [TestClass]
+    public class VectorTest
+    {
+        [TestMethod]
+        public void VectorDotProduct_WithValidVectors_ReturnsResult()
+        {
+            double[] vector1 = new double[] { 1, 2, 3 };
+            double[] vector2 = new double[] { 1, 2, 3 };
+
+            double expected = 14;
+            double actual = Vector.DotProduct(vector1, vector2);
+
+            Assert.AreEqual(expected, actual, 0.0001, "Incorrect dot product");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void VectorDotProduct_WhenVectorsSizeNotEqual_ShouldThrowArgument()
+        {
+            double[] vector1 = new double[] { 1, 2, 3 };
+            double[] vector2 = new double[] { 1, 2 };
+
+            double actual = Vector.DotProduct(vector1, vector2);
+        }
+    }
+}
