ba35d67e5419b8f16befdde152ff5bee42cd71cc
Stefan Andrei Balas <andreibalas93@gmail.com>
2015-12-08 20:07:18 +0000

    Added matrix operations

---
+++ ANN.Test/Utils/MatrixTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ANN.Utils;
+
+namespace ANN.Test.Utils
+{
+    [TestClass]
+    public class MatrixTest
+    {
+        [TestMethod]
+        public void MatrixAdd_WithValidMatrices_ReturnsResult()
+        {
+            double[][][] matrix1 = new double[][][] {
+                new double[][] {
+                    new double[] { 0, 1 },
+                    new double[] { 2, 3, 4 } },
+                new double[][] {
+                    new double[] { 5 } } };
+            double[][][] matrix2 = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 4 },
+                    new double[] { 3, 2, 1 } },
+                new double[][] {
+                    new double[] { 0 } } };
+            double[][][] expected = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 5 },
+                    new double[] { 5, 5, 5 } },
+                new double[][] {
+                    new double[] { 5 } } };
+
+            double[][][] actual = Matrix.AddMatrices(matrix1, matrix2);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                for (int j = 0; j < actual[i].Length; j++)
+                {
+                    CollectionAssert.AreEqual(expected[i][j], actual[i][j], "Incorrect matrix addition");
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MatrixAdd_WithInvalidMatricesFirst_ThrowsArgument()
+        {
+            double[][][] matrix1 = new double[][][] {
+                new double[][] {
+                    new double[] { 0, 1 },
+                    new double[] { 2, 3, 4 } },
+                new double[][] {
+                    new double[] { 5 } } };
+            double[][][] matrix2 = new double[][][] {
+                new double[][] {
+                    new double[] { 3, 2, 1} } };
+            double[][][] expected = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 5 },
+                    new double[] { 5, 5, 5 } },
+                new double[][] {
+                    new double[] { 5 } } };
+
+            double[][][] actual = Matrix.AddMatrices(matrix1, matrix2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MatrixAdd_WithInvalidMatricesSecond_ThrowsArgument()
+        {
+            double[][][] matrix1 = new double[][][] {
+                new double[][] {
+                    new double[] { 0, 1 },
+                    new double[] { 2, 3, 4 } },
+                new double[][] {
+                    new double[] { 5 } } };
+            double[][][] matrix2 = new double[][][] {
+                new double[][] {
+                    new double[] { 3, 2, 1} },
+                new double[][] {
+                    new double[] { 0 } } };
+            double[][][] expected = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 5 },
+                    new double[] { 5, 5, 5 } },
+                new double[][] {
+                    new double[] { 5 } } };
+
+            double[][][] actual = Matrix.AddMatrices(matrix1, matrix2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MatrixAdd_WithInvalidMatricesThird_ThrowsArgument()
+        {
+            double[][][] matrix1 = new double[][][] {
+                new double[][] {
+                    new double[] { 0, 1 },
+                    new double[] { 2, 3, 4 } },
+                new double[][] {
+                    new double[] { 5 } } };
+            double[][][] matrix2 = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 4 },
+                    new double[] { 3, 2} },
+                new double[][] {
+                    new double[] { 0 } } };
+            double[][][] expected = new double[][][] {
+                new double[][] {
+                    new double[] { 5, 5 },
+                    new double[] { 5, 5, 5 } },
+                new double[][] {
+                    new double[] { 5 } } };
+
+            double[][][] actual = Matrix.AddMatrices(matrix1, matrix2);
+        }
+    }
+}
---
+++ ANN/Utils/Matrix.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANN.Utils
+{
+    public class Matrix
+    {
+        public static double[][][] AddMatrices(double[][][] xs, double[][][] ys)
+        {
+            double[][][] result = xs.Select(x => x.ToArray()).ToArray();
+
+            if (xs.Length != ys.Length)
+            {
+                throw new ArgumentException("Matrices must have same dimensions.");
+            }
+
+            for (int i = 0; i < xs.Length; i++)
+            {
+                if (xs[i].Length != ys[i].Length)
+                {
+                    throw new ArgumentException("Matrices must have same dimensions.");
+                }
+            }
+
+            for (int i = 0; i < xs.Length; i++)
+            {
+                for (int j = 0; j < xs[i].Length; j++)
+                {
+                    if (xs[i][j].Length != ys[i][j].Length)
+                    {
+                        throw new ArgumentException("Matrices must have same dimensions.");
+                    }
+                }
+            }
+
+            for (int i = 0; i < xs.Length; i++)
+            {
+                for (int j = 0; j < xs[i].Length; j++)
+                {
+                    for (int k = 0; k < xs[i][j].Length; k++)
+                    {
+                        result[i][j][k] += ys[i][j][k];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
