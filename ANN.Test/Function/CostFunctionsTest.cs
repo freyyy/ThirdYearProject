bea60fda1e377fe1607dd4a5804c7a7cb2dd73b8
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-03 18:08:03 +0000

    Added cost function which includes half squared error and L2 (weight decay) regularisation. Also fixed a bug in the implementation of half squared error cost.

---
+++ ANN.Test/Function/CostFunctionsTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ANN.Function;
+using ANN.Core;
+
+namespace ANN.Test.Function
+{
+    [TestClass]
+    public class CostFunctionsTest
+    {
+        [TestMethod]
+        public void HalfSquaredErrorL2Cost_ReturnsCost()
+        {
+            ActivationFunction sigmoidFunction = new SigmoidFunction();
+            Layer[] layers = new Layer[] { new Layer(2, 2, sigmoidFunction), new Layer(1, 2, sigmoidFunction) };
+            Network network = new Network(layers);
+
+            double lambda = 0.25;
+            double[][] output = new double[][]
+            {
+                new double[] { 0.2 },
+                new double[] { 0.8 },
+                new double[] { 1.2 }
+            };
+            double[][] target = new double[][]
+            {
+                new double[] { 0 },
+                new double[] { 0.7 },
+                new double[] { 1.5 }
+            };
+            double expected = 0.14667;
+
+            layers[0][0][0] = 0.1;
+            layers[0][0][1] = 0.2;
+            layers[0][1][0] = 0.3;
+            layers[0][1][1] = 0.4;
+            layers[1][0][0] = 0.5;
+            layers[1][0][1] = 0.5;
+
+            double actual = CostFunctions.HalfSquaredErrorL2(network, lambda, output, target);
+
+            Assert.AreEqual(expected, actual, 0.0001, "Invalid squared error cost");
+        }
+
+        [TestMethod]
+        public void HalfSquaredErrorCost1D_ReturnsCost()
+        {
+            double[] output = new double[] { 0.2, 0.8, 1.2 };
+            double[] target = new double[] { 0, 0.7, 1.5 };
+            double expected = 0.07;
+            double actual = CostFunctions.HalfSquaredError(output, target);
+
+            Assert.AreEqual(expected, actual, 0.0001, "Invalid squared error cost");
+        }
+
+        [TestMethod]
+        public void HalfSquaredErrorCost_ReturnsCost()
+        {
+            double output = 1.2;
+            double target = 1;
+            double expected = 0.02;
+            double actual = CostFunctions.HalfSquaredError(output, target);
+
+            Assert.AreEqual(expected, actual, 0.0001, "Invalid squared error cost");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void HalfSquaredErrorCost_ThrowsArgument()
+        {
+            double[] output = new double[] { 0.2, 0.8, 1.2 };
+            double[] target = new double[] { 0, 0.7 };
+            double actual = CostFunctions.HalfSquaredError(output, target);
+        }
+    }
+}
--- ANN/Function/CostFunctions.cs
+++ ANN/Function/CostFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ANN.Core;
+using System;
 using System.Linq;
 
 namespace ANN.Function
@@ -11,12 +12,44 @@ namespace ANN.Function
             {
                 throw new ArgumentException("Invalid vectors for squared error cost. Make sure they are of the same length.");
             }
-            return 0.5 * actual.Zip(target, (a, t) => Math.Pow((a - t), 2)).Average();
+            return 0.5 * actual.Zip(target, (a, t) => Math.Pow((a - t), 2)).Sum();
         }
 
         public static double HalfSquaredError(double actual, double target)
         {
             return 0.5 * Math.Pow((actual - target), 2);
+        }
+
+        public static double HalfSquaredErrorL2(Network network, double lambda, double[][] actual, double[][] target)
+        {
+            if (actual.Length != target.Length)
+            {
+                throw new ArgumentException("Invalid vectors for squared error cost with L2 regularisation. Make sure they are of the same length.");
+            }
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                if (actual[i].Length != target[i].Length)
+                {
+                    throw new ArgumentException("Invalid vectors for squared error cost with L2 regularisation. Make sure they are of the same length.");
+                }
+            }
+
+            double averageSquaredError = actual.Zip(target, (a1d, t1d) => a1d.Zip(t1d, (a, t) => Math.Pow(a - t, 2)).Sum()).Average();
+            double weightDecay = 0;
+
+            for (int i = 0; i < network.LayerCount; i++)
+            {
+                for (int j = 0; j < network[i].NeuronCount; j++)
+                {
+                    for (int k = 0; k < network[i][j].InputCount; k++)
+                    {
+                        weightDecay += Math.Pow(network[i][j][k], 2);
+                    }
+                }
+            }
+
+            return averageSquaredError + (lambda / 2 * weightDecay);
         }
     }
 }
