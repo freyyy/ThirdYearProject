c156b2838c8c97104452321f506499f6008b03da
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-06 16:22:58 +0000

    Implemented method to computed average activations given cached activations in a 3D array.

--- ANN.Test/Utils/NetworksTest.cs
+++ ANN.Test/Utils/NetworksTest.cs
@@ -36,5 +36,79 @@ namespace ANN.Test.Utils
 
             Assert.AreEqual(5.4, actual, 0.0001, "Invalid weights sum");
         }
+
+        [TestMethod]
+        public void AverageActivations_ReturnsAverages()
+        {
+            ActivationFunction sigmoidFunction = new SigmoidFunction();
+            Layer[] layers = new Layer[] { new Layer(3, 2, sigmoidFunction), new Layer(2, 3, sigmoidFunction) };
+            Network network = new Network(layers);
+
+            double[][][] activations = new double[][][]
+            {
+                new double[][]
+                {
+                    new double[] { 1.2, 1.3, 1.4 },
+                    new double[] { 0.2, 0.3 }
+                },
+                new double[][]
+                {
+                    new double[] { 1.0, 1.7, 0.4 },
+                    new double[] { 0.5, 0.1 }
+                },
+                new double[][]
+                {
+                    new double[] { 1.4, 0.9, 0.1 },
+                    new double[] { 1.1, 0.9 }
+                },
+                new double[][]
+                {
+                    new double[] { 1.4, 1.3, 1.2 },
+                    new double[] { 0.5, 0.6 }
+                }
+            };
+
+            double[][] expected = new double[][]
+            {
+                new double[] { 1.25, 1.3, 0.775 },
+                new double[] { 0.575, 0.475 }
+            };
+
+            double[][] actual = Networks.AverageActivations(network, activations);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j], 0.0001, "Incorrect average activations");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void AverageActivations_ReturnsZeros()
+        {
+            ActivationFunction sigmoidFunction = new SigmoidFunction();
+            Layer[] layers = new Layer[] { new Layer(3, 2, sigmoidFunction), new Layer(2, 3, sigmoidFunction) };
+            Network network = new Network(layers);
+
+            double[][][] activations = new double[0][][];
+
+            double[][] expected = new double[][]
+            {
+                new double[] { 0, 0, 0 },
+                new double[] { 0, 0 }
+            };
+
+            double[][] actual = Networks.AverageActivations(network, activations);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j], 0.0001, "Incorrect average activations");
+                }
+            }
+        }
     }
 }
--- ANN/Utils/Networks.cs
+++ ANN/Utils/Networks.cs
@@ -26,5 +26,33 @@ namespace ANN.Utils
 
             return result;
         }
+
+        public static double[][] AverageActivations(Network network, double[][][] activations)
+        {
+            double[][] average = new double[network.LayerCount][];
+
+            for (int i = 0; i < network.LayerCount; i++)
+            {
+                average[i] = new double[network[i].NeuronCount];
+            }
+
+            if (activations.Length == 0)
+            {
+                return average;
+            }
+
+            for (int i = 0; i < activations.Length; i++)
+            {
+                for (int j = 0; j < network.LayerCount; j++)
+                {
+                    for (int k = 0; k < network[j].NeuronCount; k++)
+                    {
+                        average[j][k] += activations[i][j][k] / activations.Length;
+                    }
+                }
+            }
+
+            return average;
+        }
     }
 }
