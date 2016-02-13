32eb9e82a678098d5152a337c98023809f9d9566
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-13 12:22:37 +0000

    Added the images.csv data set of natural images to the resource list.

--- AutoencoderDemo/Program.cs
+++ AutoencoderDemo/Program.cs
@@ -2,13 +2,47 @@
 using ANN.Function;
 using ANN.Learning;
 using ANN.Utils;
+using LumenWorks.Framework.IO.Csv;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AutoencoderDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void GetSamples()
+        {
+            List<int[]> trainingData = new List<int[]>();
+            List<int> labels = new List<int>();
+
+            using (CsvReader csv =
+                   new CsvReader(new StringReader(Properties.Resources.images), true))
+            {
+                int fieldCount = csv.FieldCount;
+                Console.WriteLine("Field count: {0}", fieldCount);
+
+                string[] headers = csv.GetFieldHeaders();
+
+                while (csv.ReadNextRecord())
+                {
+                    List<int> trainingExample = new List<int>();
+
+                    labels.Add(int.Parse(csv[0]));
+                    for (int i = 1; i < fieldCount; i++)
+                    {
+                        trainingExample.Add(int.Parse(csv[i]));
+                    }
+
+                    trainingData.Add(trainingExample.ToArray());
+                }
+            }
+
+            Console.WriteLine("Total examples: {0}", trainingData.Count);
+            Console.WriteLine("Total labels:   {0}", labels.Count);
+        }
+
+        public static void Main(string[] args)
         {
             double[][] input = new double[][]
             {
@@ -54,40 +88,6 @@ namespace AutoencoderDemo
             Console.WriteLine("{0} {1}", network[0][0].Output, network[0][1].Output);
 
             Networks.ExportHiddenWeightsToBitmap(network, 600, 100, 6, 1);
-            //network.Update(new double[] { 0, 1 });
-            //Console.WriteLine("{0} {1} {2}", network[0][0].Output, network[1][0].Output, network[1][1].Output);
-            //network.Update(new double[] { 1, 0 });
-            //Console.WriteLine("{0} {1} {2}", network[0][0].Output, network[1][0].Output, network[1][1].Output);
-            //network.Update(new double[] { 1, 1 });
-            //Console.WriteLine("{0} {1} {2}", network[0][0].Output, network[1][0].Output, network[1][1].Output);
-
-            //List<int[]> trainingData = new List<int[]>();
-            //List<int> labels = new List<int>();
-
-            //using (CsvReader csv =
-            //       new CsvReader(new StringReader(Properties.Resources.kaggle_train), true))
-            //{
-            //    int fieldCount = csv.FieldCount;
-            //    Console.WriteLine("Field count: {0}", fieldCount);
-
-            //    string[] headers = csv.GetFieldHeaders();
-
-            //    while(csv.ReadNextRecord())
-            //    {
-            //        List<int> trainingExample = new List<int>();
-
-            //        labels.Add(int.Parse(csv[0]));
-            //        for(int i = 1; i < fieldCount; i++)
-            //        {
-            //            trainingExample.Add(int.Parse(csv[i]));
-            //        }
-
-            //        trainingData.Add(trainingExample.ToArray());
-            //    }
-            //}
-
-            //Console.WriteLine("Total examples: {0}", trainingData.Count);
-            //Console.WriteLine("Total labels:   {0}", labels.Count);
         }
     }
 }
