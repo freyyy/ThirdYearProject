5014805ebcb8b2884d3349d9a32728d364bf42f6
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-14 16:17:12 +0000

    Created a test bench project to visualise data that is being used to train the autoencoder.

---
+++ SamplesTestBench/Program.cs
@@ -0,0 +1,74 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplesTestBench
+{
+    public class Program
+    {
+        private static double[][] GetSamples()
+        {
+            List<double[]> samples = new List<double[]>();
+
+            using (CsvReader csv =
+                   new CsvReader(new StringReader(Properties.Resources.images), false))
+            {
+                int fieldCount = csv.FieldCount;
+                Console.WriteLine("Field count: {0}", fieldCount);
+
+                while (csv.ReadNextRecord())
+                {
+                    List<double> trainingExample = new List<double>();
+
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        trainingExample.Add(double.Parse(csv[i]));
+                    }
+
+                    samples.Add(trainingExample.ToArray());
+                }
+            }
+
+            Console.WriteLine("Total examples: {0}", samples.Count);
+            return samples.ToArray();
+        }
+
+        private static double[][] GetPatches(double[][] samples, int samplesWidth, int samplesHeight, int patchesNum, int patchesSize)
+        {
+            List<double[]> patches = new List<double[]>();
+            int patchesPerSample = patchesNum / samples.Length;
+            double[] patch;
+
+            for (int i = 0; i < samples.Length; i++)
+            {
+                for (int j = 0; j < patchesPerSample; j++)
+                {
+                    patch = new double[patchesSize * patchesSize];
+                    int leftCornerWidth = rng.Next(0, samplesWidth - patchesSize);
+                    int leftCornerHeight = rng.Next(0, samplesHeight - patchesSize);
+                    int leftCorner = leftCornerHeight * samplesWidth + leftCornerWidth;
+
+                    for (int h = 0; h < patchesSize; h++)
+                    {
+                        for (int w = 0; w < patchesSize; w++)
+                        {
+                            patch[h * patchesSize + w] = samples[i][leftCorner + w + h * samplesWidth];
+                        }
+                    }
+
+                    patches.Add(patch);
+                }
+            }
+
+            return patches.ToArray();
+        }
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
