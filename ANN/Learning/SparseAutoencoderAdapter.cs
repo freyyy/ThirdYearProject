4baf227f7bd630af660d73990003f854c86af865
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-15 20:37:12 +0000

    Barebone implementation of adapter class for interfacing with the ALGLIB library.

---
+++ ANN/Learning/SparseAutoencoderAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANN.Learning
+{
+    public class SparseAutoencoderAdapter
+    {
+        private SparseAutoencoderLearning _sparseAutoencoder;
+        private double[][] _input;
+        private double _lastValue = double.MaxValue;
+        private double _iterations = 0;
+
+        public SparseAutoencoderAdapter(SparseAutoencoderLearning sparseAutoencoder, double[][] input)
+        {
+            _sparseAutoencoder = sparseAutoencoder;
+            _input = input;
+        }
+
+        public void FunctionValueAndGradient(double[] x, ref double func, double[] grad, object obj)
+        {
+
+        }
+
+        public void PrintProgress()
+        {
+
+        }
+
+        public SparseAutoencoderLearning SparseAutoencoderLearning
+        {
+            get { return _sparseAutoencoder; }
+        }
+
+        public double[][] Input
+        {
+            get { return _input; }
+        }
+    }
+}
