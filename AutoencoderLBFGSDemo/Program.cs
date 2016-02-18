196dc397758c5c6cdf842770f7ce2f3a22dbebb0
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-18 22:06:33 +0000

    Ongoing testing using the autoencoder implementation and l-bfgs optimisation.

--- AutoencoderLBFGSDemo/Program.cs
+++ AutoencoderLBFGSDemo/Program.cs
@@ -1,15 +1,113 @@
-﻿using System;
+﻿using ANN.Core;
+using ANN.Function;
+using ANN.Learning;
+using ANN.Utils;
+using LumenWorks.Framework.IO.Csv;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutoencoderLBFGSDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Random rng = new Random();
+
+        private static double[][] GetSamples()
         {
+            List<double[]> samples = new List<double[]>();
+
+            using (CsvReader csv =
+                   new CsvReader(new StringReader(Properties.Resources.images), false))
+            {
+                int fieldCount = csv.FieldCount;
+                Console.WriteLine("Field count: {0}", fieldCount);
+
+                while (csv.ReadNextRecord())
+                {
+                    List<double> trainingExample = new List<double>();
+
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        trainingExample.Add(double.Parse(csv[i]));
+                    }
+
+                    samples.Add(trainingExample.ToArray());
+                }
+            }
+
+            Console.WriteLine("Total examples: {0}", samples.Count);
+            return samples.ToArray();
+        }
+
+        private static double[][] GetPatches(double[][] samples, int samplesWidth, int samplesHeight, int patchesNum, int patchesSize)
+        {
+            List<double[]> patches = new List<double[]>();
+            int patchesPerSample = patchesNum / samples.Length;
+            double[] patch;
+
+            for (int i = 0; i < samples.Length; i++)
+            {
+                for (int j = 0; j < patchesPerSample; j++)
+                {
+                    patch = new double[patchesSize * patchesSize];
+                    int leftCornerWidth = rng.Next(0, samplesWidth - patchesSize);
+                    int leftCornerHeight = rng.Next(0, samplesHeight - patchesSize);
+                    int leftCorner = leftCornerHeight * samplesWidth + leftCornerWidth;
+
+                    for (int h = 0; h < patchesSize; h++)
+                    {
+                        for (int w = 0; w < patchesSize; w++)
+                        {
+                            patch[h * patchesSize + w] = samples[i][leftCorner + w + h * samplesWidth];
+                        }
+                    }
+
+                    patches.Add(patch);
+                }
+            }
+
+            return patches.ToArray();
+        }
+
+        public static void Main(string[] args)
+        {
+            double[][] samples = GetSamples();
+            double[][] patches = GetPatches(samples, 512, 512, 10000, 8);
+            patches = Maths.RemoveDcComponent(patches);
+            patches = Maths.TruncateAndRescale(patches, 0.1, 0.9);
+
+            ActivationFunction f = new SigmoidFunction();
+            Layer layer1 = new Layer(25, 64, f);
+            Layer layer2 = new Layer(64, 25, f);
+            Network network = new Network(new Layer[] { layer1, layer2 });
+            SparseAutoencoderLearning sae = new SparseAutoencoderLearning(network, 10000, 1, 0.0001, 0.01, 3);
+            SparseAutoencoderAdapter saeAdapter = new SparseAutoencoderAdapter(sae, patches);
+
+            double[] x = saeAdapter.GetFunctionParameters();
+            double[] y = saeAdapter.GetFunctionParameters();
+            double epsg = 0.0000000001;
+            double epsf = 0;
+            double epsx = 0;
+            int maxits = 400;
+            alglib.mincgstate state;
+            alglib.mincgreport rep;
+
+            alglib.mincgcreate(x, out state);
+            alglib.mincgsetcond(state, epsg, epsf, epsx, maxits);
+            alglib.mincgsetxrep(state, true);
+            alglib.mincgoptimize(state, saeAdapter.FunctionValueAndGradient, saeAdapter.PrintProgress, null);
+            alglib.mincgresults(state, out x, out rep);
+
+            Console.WriteLine("{0}", rep.terminationtype);
+            //Console.WriteLine("{0}", alglib.ap.format(x, 2));
+
+            Networks.ExportHiddenWeightsToBitmap(network, 128, 128, 8, 8);
+
+            Console.ReadLine();
         }
     }
 }
