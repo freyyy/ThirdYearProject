ffd2eca77913458e775c073544bdacd2f9aa96a1
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-02-22 22:20:32 +0000

    Implemented parallel computation of cost function.

--- ANN/Function/CostFunctions.cs
+++ ANN/Function/CostFunctions.cs
@@ -2,6 +2,7 @@
 using ANN.Utils;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ANN.Function
 {
@@ -93,6 +94,59 @@ namespace ANN.Function
             }
 
             double cost = HalfSquaredErrorL2(network, lambda, actual, target);
+
+            for (int i = 0; i < averageActivations.Length; i++)
+            {
+                cost += beta * Maths.KLDivergence(sparsity, averageActivations[i]);
+            }
+
+            return cost;
+        }
+
+        public static double ParallelHalfSquaredErrorL2Sparsity(Network network, double[] averageActivations,
+            double sparsity, double lambda, double beta, double[][] actual, double[][] target)
+        {
+            double cost = 0;
+            object lockCost = new object();
+
+            Parallel.For(0, actual.Length,
+                () => 0.0d,
+
+                (x, loopState, partialResult) =>
+                {
+                    for (int i = 0; i < actual[x].Length; i++)
+                    {
+                        partialResult += 0.5 * Math.Pow((actual[x][i] - target[x][i]), 2);
+                    }
+
+                    return partialResult;
+                },
+
+                (localPartialCost) =>
+                {
+                    lock (lockCost)
+                    {
+                        cost += localPartialCost;
+                    }
+                }
+            );
+
+            cost = cost / actual.Length;
+
+            double weightDecay = 0;
+
+            for (int i = 0; i < network.LayerCount; i++)
+            {
+                for (int j = 0; j < network[i].NeuronCount; j++)
+                {
+                    for (int k = 0; k < network[i][j].InputCount; k++)
+                    {
+                        weightDecay += Math.Pow(network[i][j][k], 2);
+                    }
+                }
+            }
+
+            cost += lambda / 2 * weightDecay;
 
             for (int i = 0; i < averageActivations.Length; i++)
             {
