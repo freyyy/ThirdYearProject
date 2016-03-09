adaad7d5fbf8b7d4affdd5a7cbf9a7256f6e1ced
Stefan Andrei Balas <andreibalas93@gmail.com>
2016-03-09 22:42:21 +0000

    Added methods to explicitly load either patches or MNIST samples.

--- AutoencoderMatrixLBFGSDemo/Program.cs
+++ AutoencoderMatrixLBFGSDemo/Program.cs
@@ -20,9 +20,11 @@ namespace AutoencoderMatrixLBFGSDemo
         public static Random rng = new Random();
         public static Stopwatch s = new Stopwatch();
 
-        private static double[][] GetSamples()
+        private static Matrix<double> GetPatchesTrainSamples(int count, int size)
         {
             List<double[]> samples = new List<double[]>();
+            List<double[]> patches = new List<double[]>();
+            double[] patch;
 
             using (CsvReader csv =
                    new CsvReader(new StringReader(Properties.Resources.images), false))
@@ -42,34 +44,29 @@ namespace AutoencoderMatrixLBFGSDemo
                 }
             }
 
-            return samples.ToArray();
-        }
-
-        private static double[][] GetPatches(double[][] samples, int samplesWidth, int samplesHeight, int patchesNum, int patchesSize)
-        {
-            List<double[]> patches = new List<double[]>();
-            double[] patch;
-
-            for (int i = 0; i < patchesNum; i++)
+            for (int i = 0; i < count; i++)
             {
-                patch = new double[patchesSize * patchesSize];
-                int sample = rng.Next(0, samples.Length);
-                int leftCornerWidth = rng.Next(0, samplesWidth - patchesSize);
-                int leftCornerHeight = rng.Next(0, samplesHeight - patchesSize);
-                int leftCorner = leftCornerHeight * samplesWidth + leftCornerWidth;
+                patch = new double[size * size];
+                int sample = rng.Next(0, samples.Count);
+                int leftCornerWidth = rng.Next(0, 512 - size);
+                int leftCornerHeight = rng.Next(0, 512 - size);
+                int leftCorner = leftCornerHeight * 512 + leftCornerWidth;
 
-                for (int h = 0; h < patchesSize; h++)
+                for (int h = 0; h < size; h++)
                 {
-                    for (int w = 0; w < patchesSize; w++)
+                    for (int w = 0; w < size; w++)
                     {
-                        patch[h * patchesSize + w] = samples[sample][leftCorner + w + h * samplesWidth];
+                        patch[h * size + w] = samples[sample][leftCorner + w + h * 512];
                     }
                 }
 
                 patches.Add(patch);
             }
 
-            return patches.ToArray();
+            patches = Maths.RemoveDcComponent(patches.ToArray()).ToList();
+            patches = Maths.TruncateAndRescale(patches.ToArray(), 0.1, 0.9).ToList();
+
+            return Matrix<double>.Build.DenseOfColumnArrays(patches);
         }
 
         private static Matrix<double> GetMnistTrainSamples(int count)
@@ -99,9 +96,32 @@ namespace AutoencoderMatrixLBFGSDemo
             return Matrix<double>.Build.DenseOfColumnArrays(samples);
         }
 
-        private static Matrix<double> ConvertPatchesToMatrix(double[][] patches)
+        private static Matrix<double> GetMnistTestSamples(int count)
         {
-            return Matrix<double>.Build.DenseOfColumnArrays(patches);
+            List<double[]> samples = new List<double[]>();
+
+            using (CsvReader csv =
+                   new CsvReader(new StringReader(Properties.Resources.mnist_train), false))
+            {
+                int fieldCount = csv.FieldCount;
+
+                while (csv.ReadNextRecord())
+                {
+                    List<double> trainingExample = new List<double>();
+
+                    for (int i = 1; i < fieldCount; i++)
+                    {
+                        trainingExample.Add(double.Parse(csv[i]) / 255);
+                    }
+
+                    samples.Add(trainingExample.ToArray());
+                }
+            }
+
+            samples.Reverse();
+            samples = samples.Take(count).ToList();
+
+            return Matrix<double>.Build.DenseOfColumnArrays(samples);
         }
 
         public static void Main(string[] args)
@@ -109,26 +129,21 @@ namespace AutoencoderMatrixLBFGSDemo
             Control.UseNativeMKL();
             Console.WriteLine("Setting up experiment");
 
-            //double[][] samples = GetSamples();
-            //double[][] patches = GetPatches(samples, 512, 512, 10000, 10);
-            //patches = Maths.RemoveDcComponent(patches);
-            //patches = Maths.TruncateAndRescale(patches, 0.1, 0.9);
-            //Matrix<double> input = ConvertPatchesToMatrix(patches);
-            Matrix<double> mnist = GetMnistTrainSamples(10000);
+            Matrix<double> input = GetMnistTrainSamples(100);
 
             ActivationFunction f = new SigmoidFunction();
             Layer layer1 = new Layer(196, 784, f);
             Layer layer2 = new Layer(784, 196, f);
             Network network = new Network(new Layer[] { layer1, layer2 });
-            SparseAutoencoderMatrixLearning saem = new SparseAutoencoderMatrixLearning(network, 0.0001, 0.01, 3);
+            SparseAutoencoderMatrixLearning saem = new SparseAutoencoderMatrixLearning(network, 0.003, 0.1, 3);
 
-            SparseAutoencoderMatrixAdapter saemAdapter = new SparseAutoencoderMatrixAdapter(saem, mnist, mnist);
+            SparseAutoencoderMatrixAdapter saemAdapter = new SparseAutoencoderMatrixAdapter(saem, input, input);
 
             double[] x = saem.ParametersArray();
             double epsg = 0.0000000001;
             double epsf = 0;
             double epsx = 0;
-            int maxits = 800;
+            int maxits = 400;
             alglib.minlbfgsstate state;
             alglib.minlbfgsreport rep;
 
@@ -143,6 +158,9 @@ namespace AutoencoderMatrixLBFGSDemo
 
             saem.UpdateUnderlyingNetwork();
             Networks.ExportFiltersToBitmap(network, 14, 28, 28, 1);
+
+            Matrix<double> test = GetMnistTestSamples(100);
+            Networks.ExportReconstructionsToBitmap(network, test, 10, 10, 28, 28, 1);
         }
     }
 }
